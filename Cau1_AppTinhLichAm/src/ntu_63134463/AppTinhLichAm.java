e5abcf94d02899065beeb6638d2f70c1ea5f9d20
Lương Văn Linh <luongvanlinh.45@gmail.com>
2024-03-24 22:53:07 +0700

    Cau1_AppTinhLichAm: Update title + try...catch data

--- Cau1_AppTinhLichAm/src/ntu_63134463/AppTinhLichAm.java
+++ Cau1_AppTinhLichAm/src/ntu_63134463/AppTinhLichAm.java
@@ -25,6 +25,7 @@ public class AppTinhLichAm extends JFrame {
 	
 	public AppTinhLichAm() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Chuyển đổi năm Dương lịch qua năm Âm lịch");
 		setBounds(100, 100, 774, 611);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -83,59 +84,69 @@ public class AppTinhLichAm extends JFrame {
 	}
 	
 	void exchange() {
-		 String can = "",chi = "", amlich = "";
-		String strDuongLich = edtDuongLich.getText();
-		int soNamDuong = Integer.parseInt(strDuongLich);
-		switch (soNamDuong % 10) {
-        case 0: can = "Canh";
-            break;
-        case 1: can = "Tân";
-            break;
-        case 2: can = "Nhâm";
-            break;
-        case 3: can = "Quý";
-            break;
-        case 4: can = "Giáp";
-            break;
-        case 5: can = "Ất";
-            break;
-        case 6: can = "Bính";
-            break;
-        case 7: can = "Đinh";
-            break;
-        case 8: can = "Mậu";
-            break;
-        case 9: can = "Kỷ";
-            break;
-    }
+		
+		try {
+			String can = "",chi = "", amlich = "";
+			String strDuongLich = edtDuongLich.getText();
+			int soNamDuong = Integer.parseInt(strDuongLich);
+			if (soNamDuong <= 0) {
+				 edtAmLich.setText("Năm bạn nhập không hợp lệ!");
+			} else {
+			switch (soNamDuong % 10) {
+	        case 0: can = "Canh";
+	            break;
+	        case 1: can = "Tân";
+	            break;
+	        case 2: can = "Nhâm";
+	            break;
+	        case 3: can = "Quý";
+	            break;
+	        case 4: can = "Giáp";
+	            break;
+	        case 5: can = "Ất";
+	            break;
+	        case 6: can = "Bính";
+	            break;
+	        case 7: can = "Đinh";
+	            break;
+	        case 8: can = "Mậu";
+	            break;
+	        case 9: can = "Kỷ";
+	            break;
+	    }
 
-    switch (soNamDuong % 12) {
-        case 0: chi = "Thân";
-            break;
-        case 1: chi = "Dậu";
-            break;
-        case 2: chi = "Tuất";
-            break;
-        case 3: chi = "Hợi";
-            break;
-        case 4: chi = "Tý";
-            break;
-        case 5: chi = "Sửa";
-            break;
-        case 6: chi = "Dần";
-            break;
-        case 7: chi = "Mão";
-            break;
-        case 8: chi = "Thìn";
-            break;
-        case 9: chi = "Tỵ";
-            break;
-        case 10: chi = "Ngọ";
-            break;
-        case 11: chi = "Mùi";
-            break;
-    }
-    amlich = can + " " + chi;
-    edtAmLich.setText(amlich);
-	}
+	    switch (soNamDuong % 12) {
+	        case 0: chi = "Thân";
+	            break;
+	        case 1: chi = "Dậu";
+	            break;
+	        case 2: chi = "Tuất";
+	            break;
+	        case 3: chi = "Hợi";
+	            break;
+	        case 4: chi = "Tý";
+	            break;
+	        case 5: chi = "Sửa";
+	            break;
+	        case 6: chi = "Dần";
+	            break;
+	        case 7: chi = "Mão";
+	            break;
+	        case 8: chi = "Thìn";
+	            break;
+	        case 9: chi = "Tỵ";
+	            break;
+	        case 10: chi = "Ngọ";
+	            break;
+	        case 11: chi = "Mùi";
+	            break;
+	    	}
+	    amlich = can + " " + chi;
+	    edtAmLich.setText(amlich);
+			}
+		
+		} catch (Exception e) {
+			edtAmLich.setText("Năm bạn nhập không hợp lệ!");
+		}
+}
 }
