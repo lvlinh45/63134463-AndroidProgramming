28c1c38dd16cc2db8f558d4e61b1a5d1b04a4197
Lương Văn Linh <luongvanlinh.45@gmail.com>
2024-04-16 08:50:38 +0700

    PUSH CODE VP2

---
+++ ViewPager2RecyclerViewAdapter/app/src/main/java/linh/edu/viewpager2recyclerviewadapter/LandScapeAdapter.java
@@ -0,0 +1,84 @@
+package linh.edu.viewpager2recyclerviewadapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class LandScapeAdapter extends RecyclerView.Adapter <LandScapeAdapter.ItemLandHolder> {
+
+    Context context;
+    ArrayList<LandScape> lstData;
+
+    public LandScapeAdapter(Context _context, ArrayList<LandScape> _lstData) {
+        this.context = _context;
+        this.lstData = _lstData;
+    }
+
+    @NonNull
+    @Override
+    public ItemLandHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater cai_bom = LayoutInflater.from(context);
+        View vItem = cai_bom.inflate(R.layout.item_land, parent, false);
+        ItemLandHolder viewHolderCreated = new ItemLandHolder(vItem);
+        return viewHolderCreated;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemLandHolder holder, int position) {
+        // Lấy đối tượng hiển thị
+        LandScape lanscapeHienThi = lstData.get((position));
+
+        // trích thông tin
+        String caption = lanscapeHienThi.getLandCaption();
+        String tenFileAnh = lanscapeHienThi.getLandImageFileName();
+
+
+        // Đặt các trường thông tin của holder
+        holder.tvCaption.setText(caption);
+        // đặt ảnh
+        String packageName = holder.itemView.getContext().getPackageName();
+        int imageID = holder.itemView.getResources().getIdentifier(tenFileAnh, "mipmap", packageName);
+        holder.ivLandscape.setImageResource(imageID);
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return lstData.size();
+    }
+
+    class ItemLandHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView tvCaption;
+        ImageView ivLandscape;
+        public ItemLandHolder(@NonNull View itemView) {
+            super(itemView);
+            tvCaption = itemView.findViewById(R.id.textViewCaption);
+            ivLandscape = itemView.findViewById(R.id.imageViewLand);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int viTriDuocClick = getAdapterPosition();
+            LandScape phanTuDuocClick = lstData.get(viTriDuocClick);
+
+            // bóc thông tin
+            String ten = phanTuDuocClick.getLandCaption();
+            String tenFile = phanTuDuocClick.getLandImageFileName();
+
+            // Toast tên
+            String chuoiThongBao = "bạn vừa click vào: " + ten;
+            Toast.makeText(v.getContext(), chuoiThongBao, Toast.LENGTH_SHORT).show();
+
+        }
+    }
+}
--- ViewPager2RecyclerViewAdapter/app/src/main/java/linh/edu/viewpager2recyclerviewadapter/MainActivity.java
+++ ViewPager2RecyclerViewAdapter/app/src/main/java/linh/edu/viewpager2recyclerviewadapter/MainActivity.java
@@ -7,18 +7,47 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    LandScapeAdapter landScapeAdapter;
+    ArrayList<LandScape> viewPagerData;
+    ViewPager2 viewPager2Land;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        viewPagerData = getDataForViewPager();
+        viewPager2Land = findViewById(R.id.vp2Land);
+        landScapeAdapter = new LandScapeAdapter(this, viewPagerData);
+        viewPager2Land.setAdapter(landScapeAdapter);
+        viewPager2Land.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                super.onPageScrollStateChanged(state);
+            }
         });
+    }
+
+    ArrayList<LandScape> getDataForViewPager() {
+        ArrayList <LandScape> dsDuLieu = new ArrayList<LandScape>();
+        LandScape landScape1 = new LandScape("flagtower", "Cột cờ Hà Nội");
+        dsDuLieu.add(landScape1);
+        dsDuLieu.add(new LandScape("effel", "Tháp Effel"));
+        dsDuLieu.add(new LandScape("buckingham", "Cung điện Buckingham"));
+        return dsDuLieu;
     }
 }
