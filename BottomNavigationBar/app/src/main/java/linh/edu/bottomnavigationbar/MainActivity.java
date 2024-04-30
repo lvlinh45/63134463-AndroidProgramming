4db0bcdcde20e8af9155bda29e7d4c654608c2c0
Lương Văn Linh <luongvanlinh.45@gmail.com>
2024-04-30 13:22:57 +0700

    PUSSH BOTTOMNAV

---
+++ BottomNavigationBar/app/src/main/java/linh/edu/bottomnavigationbar/MainActivity.java
@@ -0,0 +1,23 @@
+package linh.edu.bottomnavigationbar;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
