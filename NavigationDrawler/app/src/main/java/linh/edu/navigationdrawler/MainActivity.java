5795a4f48494e5f02a4639e3c88d9ad2aa37898f
Lương Văn Linh <luongvanlinh.45@gmail.com>
2024-05-31 18:23:48 +0700

    NavigationDrawer

---
+++ NavigationDrawler/app/src/main/java/linh/edu/navigationdrawler/MainActivity.java
@@ -0,0 +1,44 @@
+package linh.edu.navigationdrawler;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    ActionBarDrawerToggle drawlerToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        drawerLayout = findViewById(R.id.drawler_layout);
+        navigationView = findViewById(R.id.nav_view);
+        drawlerToggle = new ActionBarDrawerToggle(this, drawerLayout,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(drawlerToggle);
+        drawlerToggle.syncState();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else  {
+            super.onBackPressed();
+        }
+
+    }
+}
